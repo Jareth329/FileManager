11bf799728f47cf5fd4784f7a214120cad4fc1ef
Jareth Harmon <a90a3j9j390tja90ng3a@protonmail.com>
2023-05-10 06:10:48 -0500

    maintain naming consistency of sha vs hash
    - might replace hash everywhere with sha later to reduce db size slightly

--- Globals/Database.cs
+++ Globals/Database.cs
@@ -32,14 +32,14 @@ namespace FileManager.Globals
                     //      value during the image import if the actual value does not match extension (a bit slower than doing it at once, but causes
                     //      fewer issues overall and is more compatible with the idea of multiple importers)
                     //      might also add more time variables (like lstup :: last upload time)
-                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS common(sha TEXT PK, size INT, create INT, lastw INT, fstup INT, laste INT, type INT)";
+                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS common(hash TEXT PK, size INT, create INT, lastw INT, fstup INT, laste INT, type INT)";
                     cmd.ExecuteNonQuery();
 
                     // for storing folder relationships (parents/etc) I think it is better to have those relationships be stored in a separate table
                     // with the full path as the PK; that way they can be looked up and will only be stored once (instead of having to duplicate folder
                     // parents across all rows in paths) ;; note that I could use the same approach for the folder column and change it to an INT folderId
                     // which would save some space in the database
-                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS paths(sha TEXT FK, folder TEXT, file TEXT, PK (sha, folder, file))";
+                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS paths(hash TEXT FK, folder TEXT, file TEXT, PK (hash, folder, file))";
                     cmd.ExecuteNonQuery();
                     cmd.CommandText = "CREATE INDEX IF NOT EXISTS _folder ON paths(folder)";
                     cmd.ExecuteNonQuery();
@@ -51,18 +51,18 @@ namespace FileManager.Globals
                     // mainly for grouping purposes in the UI AND also adding all possible columns to database for each tag type and allowing user to 
                     // choose which ones are enabled (disabled ones would not have their content removed from database unless user specifically chooses
                     // to do so) (but this would depend on how much additional space is required to store the empty columns)
-                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS tags(sha TEXT FK, tagId INT FK, PK (sha, tagId))";
+                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS tags(hash TEXT FK, tagId INT FK, PK (hash, tagId))";
                     cmd.ExecuteNonQuery();
 
                     // metadata specific to images; sha256 hash, [perceptual hashes]: averageHash, colorHash, differenceHash, perceptualHash, waveletHash,
                     //      [colors]: red, green, blue, yellow, cyan, fuchsia/magenta, vivid, neutral, dull, light, medium, dark, alpha
                     //      might add other tertiary colors like orange/brown/purple depending on the estimated database size increase
-                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS image(sha TEXT PK, avg INT, colr INT, dif INT, perc INT, wav INT, r INT, g INT, b INT, y INT, c INT, f INT, v INT, n INT, d INT, l INT, m INT, INT k, INT, a INT)";
+                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS image(hash TEXT PK, avg INT, colr INT, dif INT, perc INT, wav INT, r INT, g INT, b INT, y INT, c INT, f INT, v INT, n INT, d INT, l INT, m INT, INT k, INT, a INT)";
                     cmd.ExecuteNonQuery();
 
                     // might be moved to a separate database file (will have to test how it affects performance at scale)
                     // id comes first in PK because that will be what I query by
-                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS imports(id INT, sha TEXT, PK (id, sha));";
+                    cmd.CommandText = "CREATE TABLE IF NOT EXISTS imports(id INT, hash TEXT, PK (id, hash));";
                     cmd.ExecuteNonQuery();
                 }
                 return Error.OK;
