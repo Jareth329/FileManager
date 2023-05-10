560d228789fcc2d43943526bbaec8641917a3834
Jareth Harmon <a90a3j9j390tja90ng3a@protonmail.com>
2023-05-10 06:03:10 -0500

    add core.collections (will likely be renamed or removed)

---
+++ Core/Collections.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace FileManager.Core
+{
+    internal static class Collections
+    {
+        // load from .json file; will contain 
+        // not sure if will use actually; I know I will have a table that contains rows of (ext TEXT PK, cat TEXT) which will be used
+        // to determine the category of each file extension (i.e. png will be in the IMAGE category) (categories might be INT instead of TEXT)
+        internal static readonly Dictionary<string, List<string>> FileExtensions = new();
+    }
+}
