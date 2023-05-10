9424fdc9425fdb62c722e08a7a23f6e664184dbc
Jareth Harmon <a90a3j9j390tja90ng3a@protonmail.com>
2023-05-10 05:59:46 -0500

    add setting, add sqlite
    - add setting for the max batch size of an import

--- Core/Settings.cs
+++ Core/Settings.cs
@@ -12,6 +12,7 @@ namespace FileManager.Core
         internal static bool SkipPreviouslyImportedFolders { get; set; } = true;
 
         internal static ushort MaxDatabaseInsertSize { get; set; } = 1024;
+        internal static short MaxImportBatchSize { get; set; } = 1024; // need to ensure this is min 1 in settings ui spinbox
 
         internal static bool AutostartScanOnPrescanCompletion { get; set; } = false;
         internal static bool AutostartImportOnScanCompletion { get; set; } = false;
