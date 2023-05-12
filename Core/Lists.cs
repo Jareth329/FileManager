52fd4a66f6305765732056e0be3878ef5e931b59
Jareth Harmon <a90a3j9j390tja90ng3a@protonmail.com>
2023-05-12 05:32:57 -0500

    add importInfo, rename Collections
    - rename Collections to Lists
    - add list of ImportInfo to Lists
    - add ImportInfo type
        - tracks progress and completion status of imports, along with the time that they were started/completed
        - also keeps track of their name/desc/color for ui elements

--- Core/Lists.cs
+++ Core/Lists.cs
@@ -1,12 +1,15 @@
-﻿using System.Collections.Generic;
+﻿using FileManager.Core.Types;
+using System.Collections.Generic;
 
 namespace FileManager.Core
 {
-    internal static class Collections
+    internal static class Lists
     {
         // load from .json file; will contain 
         // not sure if will use actually; I know I will have a table that contains rows of (ext TEXT PK, cat TEXT) which will be used
         // to determine the category of each file extension (i.e. png will be in the IMAGE category) (categories might be INT instead of TEXT)
         internal static readonly Dictionary<string, List<string>> FileExtensions = new();
+
+        internal static readonly Dictionary<ulong, ImportInfo> Imports = new();
     }
 }
---
+++ Core/Types/ImportInfo.cs
@@ -0,0 +1,19 @@
+﻿namespace FileManager.Core.Types
+{
+    internal sealed class ImportInfo
+    {
+        internal ulong Id { get; set; }
+        internal string Name { get; set; } = string.Empty;
+        internal string Description { get; set; } = string.Empty;
+        internal uint Color { get; set; }
+
+        internal int Processed { get; set; }
+        internal int Total { get; set; }
+        internal int Success { get; set; }
+        internal int Failure { get; set; }
+
+        internal bool Complete { get; set; }
+        internal long Started { get; set; }
+        internal long Finished { get; set; }
+    }
+}
