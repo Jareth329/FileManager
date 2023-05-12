291f7196b066f5f356fc7076282638178f8d6f23
Jareth Harmon <a90a3j9j390tja90ng3a@protonmail.com>
2023-05-12 16:18:27 -0500

    remove Colors enum

--- Core/Enums.cs
+++ Core/Enums.cs
@@ -1,6 +1,4 @@
 ﻿namespace FileManager.Core.Enums
 {
-    // magenta=fuchsia, orange?, violet?, brown?
-    internal enum Colors { Red, Green, Blue, Yellow, Cyan, Fuchsia, Vivid, Neutral, Dull, Light, Medium, Dark, Alpha }
     internal enum ScanAction { Import, Skip, Blacklist }
 }
