48fd698aeb10602f3b988f76cbed30d63d3f62fb
Jareth Harmon <a90a3j9j390tja90ng3a@protonmail.com>
2023-05-12 13:36:35 -0500

    add ImageInfo type

--- Core/Enums.cs
+++ Core/Enums.cs
@@ -1,5 +1,7 @@
 ﻿namespace FileManager.Core.Enums
 {
+    // magenta=fuchsia, orange?, violet?, brown?
+    internal enum Colors { Red, Green, Blue, Yellow, Cyan, Fuchsia, Vivid, Neutral, Dull, Light, Medium, Dark, Alpha }
     internal enum Error { OK, Generic, Database, Dictionary, IO, Python }
     internal enum ScanAction { Import, Skip, Blacklist }
 }
---
+++ Core/Types/ImageInfo.cs
@@ -0,0 +1,30 @@
+﻿namespace FileManager.Core.Types
+{
+    internal sealed class ImageInfo
+    {
+        internal string Hash { get; set; } = string.Empty;
+
+        internal ulong AverageHash { get; set; }
+        internal ulong ColorHash { get; set; }
+        internal ulong DifferenceHash { get; set; }
+        internal ulong PerceptualHash { get; set; }
+        internal ulong WaveletHash { get; set; }
+
+        internal byte Red { get; set; }
+        internal byte Green { get; set; }
+        internal byte Blue { get; set; }
+        internal byte Yellow { get; set; }
+        internal byte Cyan { get; set; }
+        internal byte Fuchsia { get; set; }
+
+        internal byte Vivid { get; set; }
+        internal byte Neutral { get; set; }
+        internal byte Dull { get; set; }
+
+        internal byte Light { get; set; }
+        internal byte Medium { get; set; }
+        internal byte Dark { get; set; }
+
+        internal byte Alpha { get; set; }
+    }
+}
