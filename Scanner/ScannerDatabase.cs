1db92538fc6a7855cb9bca5ef59117b181bb8871
Jareth Harmon <a90a3j9j390tja90ng3a@protonmail.com>
2023-05-12 05:33:29 -0500

    improve naming consistency

--- Scanner/ScannerDatabase.cs
+++ Scanner/ScannerDatabase.cs
@@ -9,15 +9,16 @@ namespace FileManager.Scanner
 {
     internal static class ScannerDatabase
     {
-        internal static Error Insert(ulong scanId, IEnumerable<string> paths)
+        internal static Error Insert(ulong importId, IEnumerable<string> paths)
         {
             try
             {
-                using (var connection = new SQLiteConnection($"Data Source={Path.Combine(Settings.GetMetadataPath(), "paths.db")}"))
+                using (var connection = new SQLiteConnection($"Data Source={Path.Combine(Settings.GetMetadataPath(), "imports.db")}"))
                 {
                     var cmd = connection.CreateCommand();
-                    cmd.CommandText = "INSERT INTO paths (path, id) VALUES ($path, $id)";
-                    cmd.Parameters.AddWithValue("$id", scanId);
+                    // actual table will be (path, id, index) ;; change foreach to for () and use i for index
+                    cmd.CommandText = "INSERT INTO paths (id, path) VALUES ($id, $path)";
+                    cmd.Parameters.AddWithValue("$id", importId);
 
                     var path = cmd.CreateParameter();
                     path.ParameterName = "$path";
@@ -43,7 +44,7 @@ namespace FileManager.Scanner
             }
         }
 
-        internal static Error DeletePaths(ulong scanId)
+        internal static Error DeletePaths(ulong importId)
         {
             return Error.OK;
         }
