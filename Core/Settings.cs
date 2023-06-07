e4c322b26e9b5db7bed4cf28e07698cddbdab0e5
Jareth Harmon <a90a3j9j390tja90ng3a@protonmail.com>
2023-06-07 01:52:43 -0500

    add setting
    - add setting for default scan recursion depth
    - change default value of some other settings for testing

--- Core/Settings.cs
+++ Core/Settings.cs
@@ -9,16 +9,17 @@ namespace FileManager.Core
         internal static bool UseDefaultMetadataPath { get; set; } = true;
 
         internal static sbyte MaxScanRecursionDepth { get; set; } = -1;
+        internal static sbyte DefaultScanRecursionDepth { get; set; } = -1;
 
         internal static bool SkipPreviouslyImportedFolders { get; set; } = true;
 
         internal static ushort MaxDatabaseInsertSize { get; set; } = 1024;
         internal static short MaxImportBatchSize { get; set; } = 1024; // need to ensure this is min 1 in settings ui spinbox
 
-        internal static bool AutostartScanOnPrescanCompletion { get; set; } = false;
+        internal static bool AutostartScanOnPrescanCompletion { get; set; } = true;
         internal static bool AutostartImportOnScanCompletion { get; set; } = false;
 
-        internal static bool SqliteUseSynchronousOff { get; set; } = false;
+        internal static bool SqliteUseSynchronousOff { get; set; } = true;
 
         internal static string GetMetadataPath() => (UseDefaultMetadataPath) ? DefaultMetadataPath : MetadataPath;
 
