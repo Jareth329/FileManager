e8b651703b68e99c6eefd699779f1a2caf55e053
Jareth Harmon <a90a3j9j390tja90ng3a@protonmail.com>
2023-06-07 01:54:44 -0500

    add SettingsAccess class
    - godot does not like to interact directly with static C# objects/methods

---
+++ Globals/SettingsAccess.cs
@@ -0,0 +1,14 @@
+﻿using FileManager.Core;
+using Godot;
+
+namespace FileManager.Globals
+{
+    public partial class SettingsAccess : Node
+    {
+        // godot does not like to interact with static C# objects/methods
+        public int GetDefaultScanDepth() => Settings.DefaultScanRecursionDepth;
+        public void SetDefaultScanDepth(int depth) => Settings.DefaultScanRecursionDepth = (sbyte)depth;
+
+
+    }
+}
