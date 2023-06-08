ba8d49f05440ad632ec16228194e25e03bc4d163
Jareth Harmon <a90a3j9j390tja90ng3a@protonmail.com>
2023-06-08 18:34:19 -0500

    ui work
    - continue blocking out main ui
    - do more detailed work on thumbnail list ui

---
+++ Lister/note.gd
@@ -0,0 +1,4 @@
+extends ItemList
+
+# icon_scale can be reverted to 1.0 if I add a second itemlist as an overlay that only shows selected images (to avoid
+#	needing the extra space around thumbnails that icon_scale=0.8 provides)
