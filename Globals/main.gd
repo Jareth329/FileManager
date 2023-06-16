5b4b914b608da9aff494cffeffd178b43aa08839
Jareth Harmon <a90a3j9j390tja90ng3a@protonmail.com>
2023-06-16 00:45:37 -0500

    ui improvements
    - hide note labels when running program
    - add note about rotated labels

--- Globals/main.gd
+++ Globals/main.gd
@@ -4,8 +4,12 @@ extends Control
 @onready var vsplit_left:VSplitContainer = $margin/vbox/hbox/hsplit/left
 @onready var vsplit_right:VSplitContainer = $margin/vbox/hbox/hsplit/right
 
+# note: will need to replace the rotated text on buttons with images on TextureButtons
+#	labels really do not like to be rotated 
+
 func _ready() -> void:
 	apply_settings()
+	$notes.hide()
 
 # note: once I actually load settings from disk I will need to emit a signal once said loading has finished
 func apply_settings() -> void:
